c5200c65c79bddd41160c9191f49a42f1c0e9606
jorgeSilva <jorgelfsilva5@gmail.com>
2024-03-05 15:58:57 -0300

    [0301-3]feat - Exibição dos metodos de acessar às informações das requisições que estão sendo feitas no servidor

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,4 +1,5 @@
 import { cookies } from "next/headers"
+import { NextRequest } from "next/server"
 
 export async function GET(){
   const response = await fetch('https://api.origamid.online/conta/login', {
@@ -19,4 +20,11 @@ export async function GET(){
     
   })
   return Response.json(data)
+}
+
+export async function POST(request: NextRequest){
+  const param = request.nextUrl.searchParams.get('busca')
+  const body = await request.json()
+
+  return Response.json({ body })
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ export default async function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <p>Procure por cursos na aba Cursos.</p>
     </div>
   );
 }
