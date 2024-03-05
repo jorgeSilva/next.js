520c38060e3ceadb71ecc7c8bc870df063af3637
jorgeSilva <jorgelfsilva5@gmail.com>
2024-03-05 12:55:59 -0300

    [0301-2]feat - exibição do funcionamento do cookie e sua segurança dentro do arquivo 'route.ts' na criação da rota da api dentro do projeto

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,11 +1,22 @@
+import { cookies } from "next/headers"
+
 export async function GET(){
-  const response = await fetch('https://api.origamid.online/vendas', {
+  const response = await fetch('https://api.origamid.online/conta/login', {
+    method: 'POST',
     headers: {
-      apikey: 'ORIGAMID123456'
-    }
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      username: 'dog',
+      password: 'dog'
+    })
   })
 
-  const vendas = await response.json()
-
-  return Response.json(vendas)
+  const data = await response.json()
+  cookies().set('token', data.token, {
+    httpOnly: true,
+    secure: true,
+    
+  })
+  return Response.json(data)
 }
