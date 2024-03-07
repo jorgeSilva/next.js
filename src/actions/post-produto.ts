793b990d9f7d19c19c058251f62bedbe8e1337d6
jorgeSilva <jorgelfsilva5@gmail.com>
2024-03-07 16:41:56 -0300

    [0304-2]feat - exibição do 'form action', recurso do react para que o formulario seja dependente do server component para todo o envio

--- src/actions/post-produto.ts
+++ src/actions/post-produto.ts
@@ -12,35 +12,25 @@ type IProduto = {
   importado: 1 | 0
 }
 
-export async function postProdutos(
-  nome: string, 
-  preco: number, 
-  descricao: string, 
-  estoque: number, 
-  importado: 1 | 0
-){
-  try{
-
-    const response = await fetch('https://api.origamid.online/produtos', {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify({
-        nome,
-        preco,
-        descricao,
-        estoque,
-        importado
-      })
-    })
-    
-    await response.json() as IProduto
-
-    revalidatePath('/produto')
-    redirect('/produto')
-    
-  }catch(err){
-    return false
+export async function postProdutos(formdata: FormData){
+  const produto: IProduto = {
+    nome: formdata.get('nome') as string,
+    descricao: formdata.get('descricao') as string,
+    preco: Number(formdata.get('preco')),
+    estoque: Number(formdata.get('estoque')),
+    importado: formdata.get('importado') ? 1 : 0
   }
+  
+  const response = await fetch('https://api.origamid.online/produtos', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(produto)
+  })
+  
+  await response.json() as IProduto
+
+  revalidatePath('/produto')
+  redirect('/produto')
 }
--- src/components/form.tsx
+++ src/components/form.tsx
@@ -2,41 +2,36 @@
 
 import { postProdutos } from "@/actions/post-produto"
 import React from "react"
+import { useFormStatus } from "react-dom"
+
+export function Button(){
+  const status = useFormStatus()
+  return (
+    <button type="submit" disabled={status.pending}>Adicionar</button>
+  )
+}
 
 export default function Form() {
-  const [nome, setNome] = React.useState<string>('')
-  const [preco, setPreco] = React.useState<number>(0)
-  const [descricao, setDescricao] = React.useState<string>('')
-  const [estoque, setEstoque] = React.useState<number>(0)
-  const [importado, setImportado] = React.useState<1 | 0>(0)
-
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>){
-    e.preventDefault()
-    await postProdutos(nome, preco, descricao, estoque, importado)
-  }
-
   return (
-    <form onSubmit={handleSubmit}>
+    <form action={postProdutos}>
       <label htmlFor="nome">Nome</label>
-      <input onChange={(e) => setNome(e.target.value)} type="text" name="nome" id="nome" />
+      <input  type="text" name="nome" id="nome" />
 
       <label htmlFor="preco">Preço</label>
-      <input onChange={(e) => setPreco(+e.target.value)} type="number" name="preco" id="preco" />
+      <input  type="number" name="preco" id="preco" />
 
       <label htmlFor="descricao">Descrição</label>
-      <input onChange={(e) => setDescricao(e.target.value)} type="text" name="descricao" id="descricao" />
+      <input  type="text" name="descricao" id="descricao" />
 
       <label htmlFor="Estoque">Estoque</label>
-      <input onChange={(e) => setEstoque(+e.target.value)} type="number" name="Estoque" id="Estoque" />
+      <input  type="number" name="Estoque" id="Estoque" />
 
       <label htmlFor="importado">
-        <input onChange={(e) => {
-          setImportado(e.target.checked === true ? 1 : 0)
-        }} type="checkbox" name="importado" id="importado" />
+        <input type="checkbox" name="importado" id="importado" />
         Importado
       </label>
 
-      <button type="submit">Adicionar</button>
+      <Button />
     </form>
   )
 }
