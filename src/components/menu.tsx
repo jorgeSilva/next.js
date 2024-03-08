b72683650d00c17088235c9b6d6f66646fe1ecb5
jorgeSilva <jorgelfsilva5@gmail.com>
2024-03-08 15:42:06 -0300

    [0401-2]feat - Exibição de outros dois 'Hooks' para pegar valores da url sendo um o 'useRouter' com a sua navegação programavel e o 'useSearchParams' que é necessario o import dentro de um suspense para o funcionamento correto que busca valores dentro de parametros na url

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,26 +1,39 @@
 'use client'
 
 import Link from 'next/link';
-import { useParams, usePathname } from 'next/navigation';
-import React from 'react';
+import { useParams, usePathname, useRouter, useSearchParams } from 'next/navigation';
+import React, { Suspense } from 'react';
+
+function Busca(){
+  const search = useSearchParams()
+  const busca = search.get('busca')
+  return <div>Busca: {busca}</div>
+}
 
 export default function Menu() {
   const params = useParams()
   const pathName = usePathname()
+  const router = useRouter()
 
-  // console.log({pathName});
   React.useEffect(() => {
-    console.log('Rota mudou');
-  }, [pathName])
+    // setInterval(() => {
+    //   router.refresh()
+    // }, 5000);
+  }, [router])
 
   return (
-    <ul className="menu">
-      <li>
-        <Link href="/">Home</Link>
-      </li>
-      <li>
-        <Link href="/acoes">Ações: {params.acao}</Link>
-      </li>
-    </ul>
+    <>
+      <Suspense>
+        <Busca/> 
+      </Suspense>
+      <ul className="menu">
+        <li>
+          <Link href="/">Home</Link>
+        </li> 
+        <li>
+          <Link href="/acoes/?busca=xpt">Ações: {params.acao}</Link>
+        </li>
+      </ul>
+    </>
   );
 }
