7ecf8079af8419c4400799a0dcf373a5b5e9253c
jorgeSilva <jorgelfsilva5@gmail.com>
2024-03-11 10:51:52 -0300

    [0502]feat - exibição do modo de uso do 'css modules' que por sua vez não teve diferença alguma

--- src/app/cursos/page.tsx
+++ src/app/cursos/page.tsx
@@ -1,6 +1,8 @@
+import style from './cursos.module.css'
+
 export default async function CursosPage(){
   return (
-    <div>
+    <div className={style.cursos}>
       <h1>Front End e UI Design</h1>
       <p>A Origamid oferece cursos de Front End e UI Design</p>
     </div>
