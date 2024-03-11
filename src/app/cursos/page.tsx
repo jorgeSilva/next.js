754bc728e100f344e6f21237049752cbe8258309
jorgeSilva <jorgelfsilva5@gmail.com>
2024-03-11 10:43:22 -0300

    [0501]feat - exibição do funcionamento do arquivo externo 'layout.ts' dentro de uma rota, fazendo com que todas as sub rotas tenham essa mesma parte do arquivo 'layout.tsx'

--- src/app/cursos/page.tsx
+++ src/app/cursos/page.tsx
@@ -1,20 +1,8 @@
-import { GetCursos } from "@/api/api"
-import Link from "next/link"
-
 export default async function CursosPage(){
-  const data = await GetCursos()
   return (
     <div>
-      <h1>Cursos</h1>
-      <ul>
-      {data.map((curso) => (
-        <li key={curso.id}>
-          <Link href={`/cursos/${curso.slug}`}>
-            <h2>{curso.nome}</h2>
-          </Link>
-        </li>
-      ))}
-    </ul>
+      <h1>Front End e UI Design</h1>
+      <p>A Origamid oferece cursos de Front End e UI Design</p>
     </div>
   )
 }
