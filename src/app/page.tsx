1a133d64def67120106c6938ea5f6aff375f875f
jorgeSilva <jorgelfsilva5@gmail.com>
2024-03-11 14:57:34 -0300

    [0505]feat - exibição de como importar links externos para dentro da aplicação, aqueles links que não tem pacotes npm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import Script from "next/script";
+
 export default async function Home() {
   return (
     <div>
       <h1>Home</h1>
       <p>Procure por cursos na aba Cursos.</p>
+      <Script 
+        id="script-teste" 
+        strategy="beforeInteractive"
+        src="https://api.origamid.online/scripts/idade-legal.min.js"
+
+      />
     </div>
   );
 }
