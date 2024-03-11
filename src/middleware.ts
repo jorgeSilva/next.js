8c7ae3cf32679e701ba5aa26f25a0b25bdffe3e0
jorgeSilva <jorgelfsilva5@gmail.com>
2024-03-11 16:06:34 -0300

    [0506-2]feat - exibição do uso de 'regex' dentro do 'matcher' responsavel pelo caminho onde a função 'middleware' será executada e exemplos de redirecionamento caso não tenha token nos cookies

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest){
-  const token = request.cookies.get('token')?.value
 
-  if(token){
-    return NextResponse.next()
-  }else{
+  console.log(`${request.nextUrl.pathname}`);
+
+  const token = request.cookies.get('token')?.value
+  const response =  NextResponse.next()
+
+  if(request.nextUrl.pathname.startsWith('/entrar')){
     return NextResponse.redirect(new URL('/login', request.url))
+  }
+
+  if(!token && request.nextUrl.pathname.startsWith('/conta')){
+    return NextResponse.redirect(new URL('/login', request.url))
+  }else{
+    return response
   }
 }
 
 export const config = {
-  matcher: ['/conta']
+  matcher: ['/((?!_next|highLightTitle.png).*)']
 }
