9a1ea69bc7d7bb068e3a7780113fa30e2bd6b481
jorgeSilva <jorgelfsilva5@gmail.com>
2024-03-11 14:42:26 -0300

    [0504]feat - exibição de como fazer a importação de fontes do 'google fonts' para dentro do projeto de forma otimizada e como adiciona-la ao projeto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Menu from "@/components/menu";
+import { font_body, font_display } from "./fonts";
 
 export const metadata: Metadata = {
   title: "Origamid Next",
@@ -14,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body>
+      <body className={`${font_body.className} ${font_body.variable} ${font_display.variable}`}>
         <Menu/>
         {children}
       </body>
